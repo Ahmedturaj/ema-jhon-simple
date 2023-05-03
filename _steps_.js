608ddd381ab19fb7aa50feb8696113ec358bf73e
Sheikhtoukir <sheikhtoukirahmedturaj013@gmail.com>
2023-05-03 22:01:00 +0600

    _steps_.js removed

--- _steps_.js
+++
@@ -1,8 +0,0 @@
-/*
-*create a firebase project
-*create web app
-*npm i firebase
-*save firebase config and export
-*build> authentication> get start> enable sign in in method
-*create sign up and log in rout
-*/ 
